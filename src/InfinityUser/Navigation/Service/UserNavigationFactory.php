fadd08e4046526fafcc5c72e21ba1c264510df72
ise <recklessthief@gmail.com>
2013-04-13 16:21:44 +0100

    Added navigation factory

---
+++ src/InfinityUser/Navigation/Service/UserNavigationFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace InfinityUser\Navigation\Service;
+
+use Zend\Navigation\Service\DefaultNavigationFactory;
+
+class UserNavigationFactory extends DefaultNavigationFactory
+{
+    protected function getName()
+    {
+        return 'user';
+    }
+}
